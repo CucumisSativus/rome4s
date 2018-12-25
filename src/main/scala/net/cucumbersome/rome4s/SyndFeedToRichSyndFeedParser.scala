b0490960454f54d724501c5633b4cd4f4d35e64f
Michał Gutowski <michal@cucumbersome.net>
2018-12-25 22:21:49 +0100

    Add original responses to scala classes

--- src/main/scala/net/cucumbersome/rome4s/SyndFeedToRichSyndFeedParser.scala
+++ src/main/scala/net/cucumbersome/rome4s/SyndFeedToRichSyndFeedParser.scala
@@ -2,7 +2,6 @@ package net.cucumbersome.rome4s
 
 import java.util.{List => JList}
 
-import cats.data.NonEmptyList
 import com.rometools.rome.feed.module.DCSubject
 import com.rometools.rome.feed.synd._
 import net.cucumbersome.rome4s.RichSyndFeed._
@@ -20,8 +19,9 @@ object SyndFeedToRichSyndFeedParser {
       webMaster = Option(feed.getWebMaster),
       author = Option(feed.getAuthor),
       docs = Option(feed.getDocs),
-      links = Option(feed.getLinks).flatMap(javaListToOptionOfNel(parseSyndLink)),
-      entries = Option(feed.getEntries).flatMap(javaListToOptionOfNel(parseEntry))
+      links = javaListToOptionOfNel(parseSyndLink)(feed.getLinks),
+      entries = javaListToOptionOfNel(parseEntry)(feed.getEntries),
+      original = feed
     )
 
   private[rome4s] def parseEntry(entry: SyndEntry): RichSyndEntry =
@@ -32,36 +32,41 @@ object SyndFeedToRichSyndFeedParser {
       updateDate = Option(entry.getUpdatedDate),
       title = Option(entry.getTitle),
       description = Option(entry.getDescription).map(parseSyndContent),
-      links = Option(entry.getLinks).flatMap(javaListToOptionOfNel(parseSyndLink)),
-      contents = Option(entry.getContents).flatMap(javaListToOptionOfNel(parseSyndContent)),
-      authors = Option(entry.getAuthors).flatMap(javaListToOptionOfNel(parseSyndPerson)),
-      categories = Option(entry.getCategories).flatMap(javaListToOptionOfNel(parseSyndCategory))
+      links = javaListToOptionOfNel(parseSyndLink)(entry.getLinks),
+      contents = javaListToOptionOfNel(parseSyndContent)(entry.getContents),
+      authors = javaListToOptionOfNel(parseSyndPerson)(entry.getAuthors),
+      categories = javaListToOptionOfNel(parseSyndCategory)(entry.getCategories),
+      original = entry
     )
 
   private[rome4s] def parseDcSubject(subject: DCSubject): RichDCSubject =
     RichDCSubject(
       taxonomyUri = Option(subject.getTaxonomyUri),
-      value = Option(subject.getValue)
+      value = Option(subject.getValue),
+      original = subject
     )
 
   private[rome4s] def parseSyndPerson(person: SyndPerson): RichSyndPerson =
     RichSyndPerson(
       name = person.getName,
       uri = person.getUri,
-      email = Option(person.getEmail)
+      email = Option(person.getEmail),
+      original = person
     )
 
   private[rome4s] def parseSyndContent(content: SyndContent): RichSyndContent =
     RichSyndContent(
       `type` = content.getType,
       value = content.getValue,
-      mode = content.getMode
+      mode = content.getMode,
+      original = content
     )
 
   private[rome4s] def parseSyndCategory(category: SyndCategory): RichSyndCategory =
     RichSyndCategory(
       taxonomyUri = Option(category.getTaxonomyUri),
-      name = Option(category.getName)
+      name = Option(category.getName),
+      original = category
     )
 
   private[rome4s] def parseSyndLink(link: SyndLink): RichSyndLink =
@@ -71,13 +76,15 @@ object SyndFeedToRichSyndFeedParser {
       `type` = Option(link.getType),
       hreflang = Option(link.getHreflang),
       title = Option(link.getTitle),
-      length = link.getLength
+      length = link.getLength,
+      original = link
     )
 
   private[rome4s] def javaListToOptionOfNel[JavaModel, ScalaModel](convert: JavaModel => ScalaModel)
-                                                                  (list: JList[JavaModel]): Option[NonEmptyList[ScalaModel]] = {
-    NonEmptyList.fromList(
-      list.asScala.toList.map(convert)
-    )
+                                                                  (list: JList[JavaModel]): List[ScalaModel] = {
+    Option(list)
+      .map(_.asScala.toList)
+      .map(_.map(convert))
+      .getOrElse(List.empty)
   }
 }
